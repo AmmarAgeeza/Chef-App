0457df5109a4be4f4a807740f751f15396501810
AmmarAgeeza <ammarfathy516@gmail.com>
2023-08-22 11:19:14 +0300

    appcolors and images

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,9 @@
+class AppAssets{
+  static const String basePath='assets/images/';
+  static const String appLogo='${basePath}logo.png';
+  static const String appLogoSVG='${basePath}logo_svg.png';
+  static const String lock='${basePath}lock.png';
+  static const String imagePicker='${basePath}image_picker.png';
+  static const String background='${basePath}background.png';
+  static const String backgroundTwo='${basePath}background_two.png';
+}
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primary= Color(0xffFA9A0C); 
+  static const Color black= Color.fromRGBO(30, 30, 30, 1); 
+  static const Color white= Color(0xffFFFFFF); 
+  static const Color grey= Color(0xff9D9FA0); 
+}
