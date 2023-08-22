d32d812600ecab742ee3c854bd41af755771af47
AmmarAgeeza <ammarfathy516@gmail.com>
2023-08-22 12:36:26 +0300

    app text style

---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,36 @@
+import 'package:chef_app/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle _textStyle({
+  required Color color,
+  required double fontSize,
+  required FontWeight fontWeight,
+}) {
+  return GoogleFonts.lato(
+    color: color,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+  );
+}
+
+//bold style
+TextStyle boldStyle({
+  Color color = AppColors.white,
+  double fontSize = 24,
+}) =>
+    _textStyle(
+      color: color,
+      fontSize: fontSize,
+      fontWeight: FontWeight.bold,
+    );
+//regular style
+TextStyle regularStyle({
+  Color color = AppColors.white,
+  double fontSize = 24,
+}) =>
+    _textStyle(
+      color: color,
+      fontSize: fontSize,
+      fontWeight: FontWeight.normal,
+    );
