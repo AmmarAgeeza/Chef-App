bb1a7f7875c986b7e4dfc9e5928a660f326ef9e9
AmmarAgeeza <ammarfathy516@gmail.com>
2023-08-22 13:33:59 +0300

    app routes [2]

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../core/routes/app_routes.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: Routes.intitlRoute,
+      onGenerateRoute: AppRoutes.generateRoute,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      ),
-      home: Scaffold(
-        body: Center(child: MaterialButton(onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (_) => Scaffold()));
-        })),
       ),
     );
   }
--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,6 +1,19 @@
+import 'package:chef_app/features/auth/presentation/screens/change_lang_screen.dart';
+import 'package:chef_app/features/auth/presentation/screens/login_screen.dart';
+import 'package:chef_app/features/auth/presentation/screens/reset_password.dart';
+import 'package:chef_app/features/auth/presentation/screens/send_code_screen.dart';
+import 'package:chef_app/features/auth/presentation/screens/splash_screen.dart';
+import 'package:chef_app/features/menu/presentation/screens/add_meal_screen.dart';
+import 'package:chef_app/features/menu/presentation/screens/menu_home.dart';
+import 'package:chef_app/features/profile/presentation/screens/change_password_screen.dart';
+import 'package:chef_app/features/profile/presentation/screens/profile_home_screen.dart';
+import 'package:chef_app/features/profile/presentation/screens/setting_screen.dart';
+import 'package:chef_app/features/profile/presentation/screens/update_profile_screen.dart';
+import 'package:flutter/material.dart';
+
 class Routes {
   static const String intitlRoute = '/';
-  static const String changeLan = '/changeLan';
+  static const String changeLang = '/changeLang';
   static const String login = '/login';
   static const String sendCode = '/sendCode';
   static const String restPassword = '/restPassword';
@@ -12,4 +25,41 @@ class Routes {
   static const String changePassword = '/changePassword';
 }
 
-class AppRoutes {}
+class AppRoutes {
+  static Route? generateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.intitlRoute:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+
+      case Routes.changeLang:
+        return MaterialPageRoute(builder: (_) => const ChangeLangScreen());
+      case Routes.login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case Routes.sendCode:
+        return MaterialPageRoute(builder: (_) => const SendCodeScreen());
+      case Routes.restPassword:
+        return MaterialPageRoute(builder: (_) => const ResetPasswordScreen());
+
+      case Routes.menuHome:
+        return MaterialPageRoute(builder: (_) => const MenuHomeScreen());
+      case Routes.addMeal:
+        return MaterialPageRoute(builder: (_) => const AddMealScreen());
+
+      case Routes.profileHome:
+        return MaterialPageRoute(builder: (_) => const ProfileScreen());
+      case Routes.updateProfile:
+        return MaterialPageRoute(builder: (_) => const UpdateProfileScreen());
+      case Routes.setting:
+        return MaterialPageRoute(builder: (_) => const SettingsScreen());
+      case Routes.changePassword:
+        return MaterialPageRoute(builder: (_) => const ChangePasswordScreen());
+      default:
+        return MaterialPageRoute(
+            builder: (_) => const Scaffold(
+                  body: Center(
+                    child: Text('No Found Route'),
+                  ),
+                ));
+    }
+  }
+}
