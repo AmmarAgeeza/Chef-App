d8f301c58c64f186cd5a63c04303f672eba08b4f
AmmarAgeeza <ammarfathy516@gmail.com>
2023-08-22 12:44:54 +0300

    app theme

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../utils/app_colors.dart';
+import '../utils/app_text_style.dart';
+
+ThemeData getAppTheme() {
+  return ThemeData(
+    primaryColor: AppColors.primary,
+    //scaffoldBackgroundColor
+    scaffoldBackgroundColor: AppColors.white,
+    //appBar theme
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.primary,
+    ),
+    //text theme
+    textTheme: TextTheme(
+      displayLarge: boldStyle(),
+      displayMedium: regularStyle(),
+    ),
+    //button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    ),
+    //text field
+    inputDecorationTheme: InputDecorationTheme(
+      //enabled border
+      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+      //fouced border
+      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+      //hint
+      hintStyle: boldStyle(color: AppColors.grey, fontSize: 16),
+      //fill color
+      // fillColor: AppColors.lightBlack,
+      // filled: true,
+    ),
+  );
+}
