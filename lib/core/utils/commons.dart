d651899d79e67a1cc89ff0ae80cf3ef566989503
AmmarAgeeza <ammarfathy516@gmail.com>
2023-08-23 18:57:31 +0300

    navigate after 3 sec

---
+++ lib/core/utils/commons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void navigate({
+  required BuildContext context,
+  required String route,
+  dynamic arg,
+}) {
+  Navigator.pushNamed(
+    context,
+    route,
+    arguments: arg,
+  );
+}
--- lib/features/auth/presentation/screens/splash_screen.dart
+++ lib/features/auth/presentation/screens/splash_screen.dart
@@ -1,11 +1,30 @@
+import 'package:chef_app/core/utils/commons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../core/routes/app_routes.dart';
 import '../../../../core/utils/app_assets.dart';
 import '../../../../core/utils/app_colors.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    natvigateAfterThreeSeconds();
+    super.initState();
+  }
+
+  void natvigateAfterThreeSeconds() {
+    Future.delayed(const Duration(seconds: 3)).then((value) {
+      navigate(context: context, route: Routes.changeLang);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
