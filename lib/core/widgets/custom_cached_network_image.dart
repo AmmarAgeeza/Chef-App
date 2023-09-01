f02ffc186c24c40edfd6ed4c091403cfd154c730
AmmarAgeeza <ammarfathy516@gmail.com>
2023-09-01 11:29:07 +0300

    add shimmer

--- lib/core/widgets/custom_cached_network_image.dart
+++ lib/core/widgets/custom_cached_network_image.dart
@@ -1,19 +1,34 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomCachedNetworkImage extends StatelessWidget {
   const CustomCachedNetworkImage({
-    super.key, required this.imgUrl, this.fit,
+    super.key,
+    required this.imgUrl,
+    this.fit,
   });
-final String imgUrl;
-final BoxFit? fit;
+  final String imgUrl;
+  final BoxFit? fit;
   @override
   Widget build(BuildContext context) {
     return CachedNetworkImage(
       fit: fit,
-        imageUrl: imgUrl,
-        placeholder: (context, url) => const CircularProgressIndicator(),
-        errorWidget: (context, url, error) => const Icon(Icons.error),
-     );
+      imageUrl: imgUrl,
+      placeholder: (context, url) => Shimmer.fromColors(
+        baseColor: Colors.grey[850]!,
+        highlightColor: Colors.grey[800]!,
+        child: Container(
+          height: 60.h,
+          width: 60.w,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
+      errorWidget: (context, url, error) => const Icon(Icons.error),
+    );
   }
 }
