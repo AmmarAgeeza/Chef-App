b80abaeffbc0cd03c2eeac371e0f80532b71e75e
AmmarAgeeza <ammarfathy516@gmail.com>
2023-09-01 11:43:15 +0300

    add alert dialog

---
+++ lib/core/widgets/custom_alert_dialog.dart
@@ -0,0 +1,49 @@
+import 'package:chef_app/core/locale/app_locale.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/app_colors.dart';
+import '../utils/app_strings.dart';
+
+class CustomAlertDialog extends StatelessWidget {
+  const CustomAlertDialog({
+    super.key, required this.message, required this.confirmAction,
+  });
+final String message;
+final VoidCallback confirmAction;
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      //content
+      content: Text(
+        message,
+        style: Theme.of(context)
+            .textTheme
+            .displayLarge!
+            .copyWith(color: AppColors.black),
+      ),
+      //actions
+      actions: [
+        //cancel text
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text(
+            AppStrings.cancel.tr(context),
+            style: Theme.of(context).textTheme.displayMedium!
+            .copyWith(color: AppColors.black),
+          ),
+        ),
+        //confirm text
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            AppStrings.ok.tr(context),
+            style: Theme.of(context).textTheme.displayMedium!
+            .copyWith(color: AppColors.black),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/menu/presentation/components/menu_item_component.dart
+++ lib/features/menu/presentation/components/menu_item_component.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/utils/app_colors.dart';
+import '../../../../core/widgets/custom_alert_dialog.dart';
 import '../../../../core/widgets/custom_cached_network_image.dart';
 
 class MenuItemComponent extends StatelessWidget {
@@ -21,9 +22,7 @@ class MenuItemComponent extends StatelessWidget {
             imgUrl:
                 'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-1640772.jpg&fm=jpg',
           ),
-          // Image.network(
-          //   'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-1640772.jpg&fm=jpg',
-          // ),
+          
         ),
         SizedBox(
           width: 8.w,
@@ -39,7 +38,16 @@ class MenuItemComponent extends StatelessWidget {
 
         const Spacer(),
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return CustomAlertDialog(
+                    message: 'حذف الوجبة',
+                    confirmAction: () {},
+                  );
+                });
+          },
           icon: const Icon(
             Icons.cancel,
             color: AppColors.red,
